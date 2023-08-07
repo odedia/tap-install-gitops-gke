cc8112dde33264ab2f50c71293857b00bab7b2a8
Oded Shopen <odedia@gmail.com>
2023-08-07 16:48:34 +0300

    upgrade for gitops 1.6

--- setup-repo.sh
+++ setup-repo.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+##
+
 set -o errexit -o nounset -o pipefail
 #set -o xtrace
 
@@ -21,8 +23,8 @@ fi
 cluster_name="$1"
 secrets_manager="$2"
 catalog_dir=".catalog"
-tanzu_sync_version="0.0.2"
-tap_version="1.5.0"
+tanzu_sync_version="${TS_VERSION:-0.1.0}"
+tap_version="${TAP_VERSION:-1.5.0}"
 
 if [[ ! ",eso,sops," =~ (,${secrets_manager},) ]]; then
   usage
@@ -39,6 +41,10 @@ mkdir -p clusters/"${cluster_name}"/tanzu-sync/{app,bootstrap,scripts}
 cp -R ${catalog_dir}/tanzu-sync/${tanzu_sync_version}/${secrets_manager}/scripts/* clusters/"${cluster_name}"/tanzu-sync/scripts/
 cp -R ${catalog_dir}/tanzu-sync/${tanzu_sync_version}/${secrets_manager}/bootstrap/* clusters/"${cluster_name}"/tanzu-sync/bootstrap/
 
+if [[ -d "clusters/${cluster_name}/tanzu-sync/app/config/.tanzu-managed" ]]; then
+  rm -r clusters/"${cluster_name}"/tanzu-sync/app/config/.tanzu-managed/*
+fi
+
 mkdir -p clusters/"${cluster_name}"/tanzu-sync/app/config/.tanzu-managed
 mkdir -p clusters/"${cluster_name}"/tanzu-sync/app/values
 
@@ -46,6 +52,11 @@ cp -R ${catalog_dir}/tanzu-sync/${tanzu_sync_version}/${secrets_manager}/config/
 
 # Setup cluster-config directory
 mkdir -p clusters/"${cluster_name}"/cluster-config/{config,values}
+
+if [[ -d "clusters/${cluster_name}/cluster-config/config/tap-install/.tanzu-managed" ]]; then
+  rm -r clusters/"${cluster_name}"/cluster-config/config/tap-install/.tanzu-managed/*
+fi
+
 mkdir -p clusters/"${cluster_name}"/cluster-config/config/tap-install/.tanzu-managed
 
 cp -R ${catalog_dir}/tap-install/${tap_version}/${secrets_manager}/config/* clusters/"${cluster_name}"/cluster-config/config/tap-install/.tanzu-managed
